cec1774d842bc753ace634abf0dee8adac657617
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-09-19 16:55:42 -0300

    teste

--- app/controller/emailSend.js
+++ app/controller/emailSend.js
@@ -2,7 +2,7 @@ module.exports.confirmEmail = function(app,req,res,id){
   if (req.session.verificarEmail && id) {
     //https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
     const sgMail = require('@sendgrid/mail');
-    var api = "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";
+    var api = "";
     sgMail.setApiKey(process.env.SENDGRID_API_KEY || api);
     const msg = {
       to: req.session.email,
