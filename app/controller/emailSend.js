b8233dedad4705754e1ec1a914a2f6613a991a27
Matheus Roberto J. Batista <matheus_roberto_batista@hotmail.com>
2018-09-24 17:10:44 -0300

    heroku config

--- app/controller/emailSend.js
+++ app/controller/emailSend.js
@@ -2,13 +2,13 @@ module.exports.confirmEmail = function(app,req,res,id){
   if (req.session.verificarEmail && id) {
     //https://github.com/sendgrid/sendgrid-nodejs/tree/master/packages/mail
     const sgMail = require('@sendgrid/mail');
-    var api = "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";
+    var api = "";
     sgMail.setApiKey(process.env.SENDGRID_API_KEY || api);
     const msg = {
       to: req.session.email,
       from: 'Portal de notícias<matheusnoticiaportal@gmail.com>',
       subject: 'Confirmação de e-mail',
-      html: '<strong>Obrigado por se registar em nosso portal de notícias. Segue o link(localhost:5000/validarConta?validacao='+id+') para confirmar o email. <br><br><br>PS: ignorar está url -></strong>',
+      html: '<strong>Obrigado por se registar em nosso portal de notícias. Segue o link(https://matheus-noticia.herokuapp.com?validacao='+id+') para confirmar o email. <br><br><br>PS: ignorar está url -></strong>',
     };
     sgMail.send(msg);
   }
